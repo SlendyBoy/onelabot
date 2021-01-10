f927d960cf81834c3102855c57c2a7b150466a7c
Tanguy Frouin <tanguy.frouin@etu.univ-lyon1.fr>
2021-01-10 22:04:15 +0100

    open help links in new tab (opens 2 new tabs in FF for some reason)

--- onelabot.js
+++ onelabot.js
@@ -47,7 +47,7 @@ const bot = function () {
                 button.innerHTML = option.text;
                 if (option.url) {
                     button.href = option.url;
-                    //button.target = '_blank';
+                    button.target = '_blank';
                 } else {
                     button.dataset.next = option.next;
                 }
